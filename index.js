0a8138ed4d05209aac7644acb3f90e08aa1ae421
Muhammad S Ziyad <muhammadsziyad@gmail.com>
2025-06-10 09:12:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// server.js (CommonJS version)
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-// Read cert and key
+// Load SSL key and certificate
 const sslOptions = {
   key: fs.readFileSync(path.join(__dirname, 'key.pem')),
   cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
@@ -13,15 +12,27 @@ const sslOptions = {
 // Create Express app
 const app = express();
 
+// GET /test
 app.get('/test', (req, res) => {
-  res.status(200).send('Hello from node');
+  res.send('Hello from GET /test');
 });
 
+// GET /test2
+app.get('/test2', (req, res) => {
+  res.send('Hello from GET /test2');
+});
+
+// POST /test
+app.post('/test', (req, res) => {
+  res.send('Hello from POST /test');
+});
+
+// All other routes
 app.use((req, res) => {
   res.status(404).send('404 not found');
 });
 
-// Start HTTPS server
-https.createServer(sslOptions, app).listen(4433, () => {
-  console.log('HTTPS server running at https://sample-server1234.com:4433');
+// Start HTTPS server on port 8001
+https.createServer(sslOptions, app).listen(8001, () => {
+  console.log('HTTPS server running at https://sample-server1234.com:8001');
 });
